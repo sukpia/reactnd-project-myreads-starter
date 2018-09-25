57c532cd2b23de0fb9ba3cb2393d320acd96afda
Terri Chong <terrichong@gmail.com>
2018-09-24 22:33:41 -0700

    change key to book.id

--- src/ListCurrentlyReading.js
+++ src/ListCurrentlyReading.js
@@ -11,7 +11,7 @@ class ListCurrentlyReading extends Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
             {books.filter(book => book.shelf === "currentlyReading").map((book) => (
-              <li key={book.title}>
+              <li key={book.id}>
                 <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
--- src/ListRead.js
+++ src/ListRead.js
@@ -10,7 +10,7 @@ class ListRead extends Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
             {books.filter(book => book.shelf === "read").map((book) => (
-              <li key={book.title}>
+              <li key={book.id}>
                 <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
