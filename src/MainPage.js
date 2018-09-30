150cd6c19212e98457985065bb4ce5ba0591ec6e
Terri Chong <terrichong@gmail.com>
2018-09-30 14:12:46 -0700

    chnage MainPage to Functional Component

--- src/MainPage.js
+++ src/MainPage.js
@@ -3,63 +3,57 @@ import Book from './Book'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
-class MainPage extends Component {
-
-	render() {
-		const { books, onChangeShelf } = this.props
-		// console.log(books);
-
-		return (
-			<div className="list-books">
-				<div className="list-books-title">
-	              <h1>MyReads</h1>
-	            </div>
-	            <div className="list-books-content">
-	            	<div>
-	            		<div className="bookshelf">
-					        <h2 className="bookshelf-title">Currently Reading</h2>
-					        <div className="bookshelf-books">
-					          <ol className="books-grid">
-					            {books.filter(book => book.shelf === "currentlyReading").map((book) => (
-					              <li key={book.id}>
-					                <Book book={book} onChangeShelf={onChangeShelf} displayShelf = "currentlyReading"/>
-					              </li>
-					            ))}
-					          </ol>
-					        </div>
-				      	</div>
-				      	<div className="bookshelf">
-					        <h2 className="bookshelf-title">Want to Read</h2>
-					        <div className="bookshelf-books">
-					          <ol className="books-grid">
-					            {books.filter(book => book.shelf === "wantToRead").map((book) => (
-					              <li key={book.id}>
-					                <Book book={book} onChangeShelf={onChangeShelf} displayShelf="wantToRead"/>
-					              </li>
-					            ))}
-					          </ol>
-					        </div>
-				      	</div>
-				      	<div className="bookshelf">
-					        <h2 className="bookshelf-title">Read</h2>
-					        <div className="bookshelf-books">
-					          <ol className="books-grid">
-					            {books.filter(book => book.shelf === "read").map((book) => (
-					              <li key={book.id}>
-					                <Book book={book} onChangeShelf={onChangeShelf} displayShelf="read"/>
-					              </li>
-					            ))}
-					          </ol>
-					        </div>
-				      	</div>
-				      	<div className="open-search">
-				      	  <Link to="/search">Add a Book</Link>
-			            </div>
-	            	</div>
-	            </div>
-			</div>
-		)
-	}
+function MainPage (props) {
+	return (
+		<div className="list-books">
+			<div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+      	<div>
+      		<div className="bookshelf">
+	        <h2 className="bookshelf-title">Currently Reading</h2>
+	        <div className="bookshelf-books">
+	          <ol className="books-grid">
+	            {props.books.filter(book => book.shelf === "currentlyReading").map((book) => (
+	              <li key={book.id}>
+	                <Book book={book} onChangeShelf={props.onChangeShelf} displayShelf = "currentlyReading"/>
+	              </li>
+	            ))}
+	          </ol>
+	        </div>
+      	</div>
+      	<div className="bookshelf">
+	        <h2 className="bookshelf-title">Want to Read</h2>
+	        <div className="bookshelf-books">
+	          <ol className="books-grid">
+	            {props.books.filter(book => book.shelf === "wantToRead").map((book) => (
+	              <li key={book.id}>
+	                <Book book={book} onChangeShelf={props.onChangeShelf} displayShelf="wantToRead"/>
+	              </li>
+	            ))}
+	          </ol>
+	        </div>
+      	</div>
+      	<div className="bookshelf">
+	        <h2 className="bookshelf-title">Read</h2>
+	        <div className="bookshelf-books">
+	          <ol className="books-grid">
+	            {props.books.filter(book => book.shelf === "read").map((book) => (
+	              <li key={book.id}>
+	                <Book book={book} onChangeShelf={props.onChangeShelf} displayShelf="read"/>
+	              </li>
+	            ))}
+	          </ol>
+	        </div>
+      	</div>
+      	<div className="open-search">
+      	  <Link to="/search">Add a Book</Link>
+          </div>
+      	</div>
+      </div>
+		</div>
+	)
 }
 
 export default MainPage
